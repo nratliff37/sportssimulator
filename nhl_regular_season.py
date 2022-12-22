9444a5ae1c282053c8612702399ed8c23e49fa73
Nick Ratliff <ratliff.n@northeastern.edu>
2022-12-21 23:02:22 -0500

    Fixed schedule and started parsing through games

--- nhl_regular_season.py
+++ nhl_regular_season.py
@@ -1,5 +1,28 @@
 import argparse
 
+TEAMS = {}
+
+# Function Definitions
+def create_teams():
+    teams_file = open("nhl_teams.txt", "r")
+
+    for team in teams_file:
+        team_pair = team.split(", ")
+        abv = team_pair[0]
+        name = team_pair[1].rstrip('\n')
+
+        TEAMS[abv] = {}
+        TEAMS[abv]["name"] = name
+        TEAMS[abv]["wins"] = 0
+        TEAMS[abv]["losses"] = 0
+        TEAMS[abv]["otl"] = 0
+
+    teams_file.close()
+
+def play_game(away_team, home_team):
+    print(away_team, home_team)
+
+# Main Script
 parser = argparse.ArgumentParser()
 parser.add_argument('-s', '--schedule' , help='the text file for the schedule')
 
@@ -10,25 +33,23 @@ except:
     print("No schedule given.")
     exit()
 
-TEAMS = {}
-
-def create_teams():
-    teams_file = open("nhl_teams.txt", "r")
-
-    for team in teams_file:
-        team_pair = team.split(", ")
-        abv = team_pair[0]
-        name = team_pair[1].rstrip('\n')
-
-    teams_file.close()
+# Create the teams
+create_teams()
 
 # Read the schedule file and parse through the games
 schedule_file = open(schedule, "r")
 
-for x in schedule_file:
-    pass
+it = 0
+for game in schedule_file:
+    if it > 14:
+        break
+
+    game_teams = game.split(" @ ")
+    away_team = game_teams[0]
+    home_team = game_teams[1].rstrip('\n')
+
+    play_game(away_team, home_team)
+
+    it += 1
 
 schedule_file.close()
-
-create_teams()
-print(TEAMS)
