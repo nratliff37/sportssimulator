76b651bbb12d26a6d99ce5dc529186d54e55d360
Nick Ratliff <ratliff.n@northeastern.edu>
2022-12-21 22:40:01 -0500

    Started script and created team list

--- nhl_regular_season.py
+++ nhl_regular_season.py
@@ -10,4 +10,25 @@ except:
     print("No schedule given.")
     exit()
 
+TEAMS = {}
+
+def create_teams():
+    teams_file = open("nhl_teams.txt", "r")
+
+    for team in teams_file:
+        team_pair = team.split(", ")
+        abv = team_pair[0]
+        name = team_pair[1].rstrip('\n')
+
+    teams_file.close()
+
+# Read the schedule file and parse through the games
 schedule_file = open(schedule, "r")
+
+for x in schedule_file:
+    pass
+
+schedule_file.close()
+
+create_teams()
+print(TEAMS)
