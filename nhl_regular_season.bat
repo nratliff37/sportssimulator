ec3173aef41500edce71adddc76bf693bc1a15f6
nratliff37 <48270053+nratliff37@users.noreply.github.com>
2023-03-13 13:23:53 -0400

    Update nhl_regular_season.bat

--- nhl_regular_season.bat
+++ nhl_regular_season.bat
@@ -1,3 +1,4 @@
 @echo off
+Rem These file paths are for my (the writer's) PC system. Change paths to fit yours for this to work.
 "C:\Users\Nick Ratliff\AppData\Local\Programs\Python\Python39\python.exe" "D:\Nick's Stuff\Code Projects\nhlsimulator\nhl_regular_season.py" -s 2223
 pause
