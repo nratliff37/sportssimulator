ed7eb7a7672130ce440033f1f2f051387ceaa34b
Nick Ratliff <ratliff.n@northeastern.edu>
2023-11-02 23:22:23 -0400

    Edited README and finished announcement implementation of NBA draft lottery

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ A program to simulate regular seasons.
 Currently included is the NHL, and MLW. (MLB, NBA, and NFL coming soon)
 
 Under mariosimulator there are scripts to run MBA and MHL.
+
+tools directory includes a script to calculate playoff series odds and an NBA Draft Lottery simulator.
--- tools/draft_lottery/nba_draft_lottery.py
+++ tools/draft_lottery/nba_draft_lottery.py
@@ -1,5 +1,6 @@
 import itertools
 import random
+import time
 
 BALLS = []
 LOTTERY_ORDER = []
@@ -64,11 +65,27 @@ def get_nth_string(num):
     else:
         return str(num) + "th"
 
+def print_top_four():
+    out_string = "The top 4 is:"
+
+    it = 1
+    for team in TOP_FOUR:
+        out_string += (" " + team)
+
+        if it != len(TOP_FOUR):
+            out_string += ","
+
+        it += 1
+
+    print(out_string)
+
 # Main Script
 
-for i in range(0, 14):
+for i in range(14):
     BALLS.append(i + 1)
-    LOTTERY_ORDER.append("Team" + str(i + 1))
+
+    team = input("Team " + str(i + 1) + ": ")
+    LOTTERY_ORDER.append(team)
 
 comb_num = 0
 for comb in itertools.combinations(BALLS, 4):
@@ -103,16 +120,26 @@ for team in LOTTERY_ORDER:
         DRAFT_ORDER.append(team)
         
 # Announce the order
-print(DRAFT_ORDER)
 announce_num = 14
 expected_team_num = 13
 TOP_FOUR = []
-while announce_num > 0:
+print("Beginning the order announcement.")
+while announce_num > 4:
+    if len(TOP_FOUR) == 4:
+        nth_string = get_nth_string(announce_num)
+        announce_num -= 1
+        announce_team = DRAFT_ORDER[announce_num]
+        print(nth_string + " Pick: " + announce_team)
+        continue
+
+    time.sleep(5)
     nth_string = get_nth_string(announce_num)
     announce_num -= 1
     
     announce_team = DRAFT_ORDER[announce_num]
-    print("The " + nth_string + " pick will go to the " + announce_team)
+    print("The " + nth_string + " pick will go to", end="", flush=True)
+    time.sleep(3)
+    print(" the " + announce_team)
     
     if announce_num >= 4:
         expected_team = LOTTERY_ORDER[expected_team_num]
@@ -120,19 +147,49 @@ while announce_num > 0:
         
         if announce_team != expected_team:
             jumpgap = 0
-            print(expected_team + " is in the Top 4!")
+            time.sleep(3)
+            print("The " + expected_team + " are in the Top 4!")
             while announce_team != expected_team:
                 jumpgap += 1
                 if jumpgap > 1:
-                    print(expected_team + " is ALSO in the Top 4!")
+                    time.sleep(3)
+                    print("The " + expected_team + " are ALSO in the Top 4!")
                 
                 TOP_FOUR.append(expected_team)
+                print_top_four()
                 expected_team = LOTTERY_ORDER[expected_team_num]
                 expected_team_num -= 1
-    else:
+
+                if len(TOP_FOUR) == 4 and announce_num > 4:
+                    time.sleep(5)
+                    print("The Top 4 has been determined. The remaining draft order is:")
+                    time.sleep(3)
+    
+    if announce_num == 4:
         while expected_team_num >= 0:
             expected_team = LOTTERY_ORDER[expected_team_num]
+            time.sleep(3)
+            print("The " + expected_team + " are in the Top 4.")
             TOP_FOUR.append(expected_team)
             expected_team_num -= 1
-            
-print(TOP_FOUR)
+            print_top_four()
+
+time.sleep(5)
+print("Announcing the order of the Top 4.")
+while announce_num > 0:
+    time.sleep(5)
+    nth_string = get_nth_string(announce_num)
+    announce_num -= 1
+    
+    announce_team = DRAFT_ORDER[announce_num]
+    print("The " + nth_string + " pick will go to", end="", flush=True)
+    time.sleep(3)
+    print(" the " + announce_team)
+
+time.sleep(5)
+print("The Final Draft Order is:")
+time.sleep(5)
+for i in range(14):
+    team = DRAFT_ORDER[i]
+    print(str(i + 1) + ". " + team)
+    
