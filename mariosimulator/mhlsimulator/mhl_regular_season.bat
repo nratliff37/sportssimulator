71cf52234a5bf97534c6b473140eb070b047324b
Nick Ratliff <ratliff.n@northeastern.edu>
2024-08-01 22:48:08 -0400

    Fixed batch files

--- mariosimulator/mhlsimulator/mhl_regular_season.bat
+++ mariosimulator/mhlsimulator/mhl_regular_season.bat
@@ -1,4 +1,4 @@
 @echo off
 Rem These file paths are for my (the writer's) PC system. Change paths to fit yours for this to work.
-"C:\Users\Nick Ratliff\AppData\Local\Programs\Python\Python39\python.exe" "D:\Nick's Stuff\Code Projects\sportssimulator\mariosimulator\mhl_regular_season.py" -s mhl
+"C:\Users\Nick Ratliff\AppData\Local\Programs\Python\Python39\python.exe" "D:\Nick's Stuff\Code Projects\sportssimulator\mariosimulator\mhlsimulator\mhl_regular_season.py" -s mhl
 pause
